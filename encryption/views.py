9fec426547b0e5400840143bce21da0837423a21
Ilya Zaytsev <58442219+meltoroun@users.noreply.github.com>
2023-10-30 16:11:51 +0300

    Внесены правки в алгоритмы шифрования
    Принудительно переводиться в кодировку utf? сам алгоритм вместо ASCII обрабатывает как utf с обработкой исключений

--- encryption/views.py
+++ encryption/views.py
@@ -4,24 +4,28 @@ from encryption.utils import encrypt, decrypt
 
 from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
+import io
 
-
-# Constants
 ALPHABET_POWER = 26
+
+
 def encrypt_file(request):
     if request.method == 'POST' and request.FILES['myfile']:
         myfile = request.FILES['myfile']
         key = int(request.POST['key'])
 
         encrypted_text = ''
-        for line in myfile:
-            for char in line:
-                ascii_val = int(char)
-                encrypted_char = chr((ascii_val + key) % ALPHABET_POWER)
-                encrypted_text += encrypted_char
+        with io.BytesIO(myfile.read()) as file:
+            lines = file.readlines()
+            for line in lines:
+                stripped_line = line.strip().decode('utf-8')  # декодирование из байтов в текст
+                for char in stripped_line:
+                    if char.isalpha():
+                        encrypted_text += chr((ord(char) + key - 65) % ALPHABET_POWER + 65)
+                    else:
+                        encrypted_text += char
 
-        # Save encrypted text to a new file
-        with open(myfile.name.split('.')[0] + '.cry', 'w') as encrypted_file:
+        with open(myfile.name + '.cry', 'wt', encoding='utf-8') as encrypted_file:
             encrypted_file.write(encrypted_text)
 
         return render(request, 'encryption/encryption_success.html', {
@@ -36,14 +40,19 @@ def decrypt_file(request):
         key = int(request.POST['key'])
 
         decrypted_text = ''
-        for line in myfile:
-            for char in line:
-                ascii_val = int(char)
-                decrypted_char = chr((ascii_val - key) % ALPHABET_POWER)
-                decrypted_text += decrypted_char
+        with io.BytesIO(myfile.read()) as file:
+            lines = file.readlines()
+            for line in lines:
+                stripped_line = line.strip().decode('utf-8')  # декодирование из байтов в текст
+                for char in stripped_line:
+                    if char.isalpha():
+                        char_code = ord(char)
+                        base = ord('a') if char.islower() else ord('A')
+                        decrypted_text += chr((char_code - base - key) % ALPHABET_POWER + base)
+                    else:
+                        decrypted_text += char
 
-        # Save decrypted text to a new file
-        with open(myfile.name.split('.')[0] + '.txt', 'w') as decrypted_file:
+        with open(myfile.name.split('.')[0] + '.txt', 'wt', encoding='utf-8') as decrypted_file:
             decrypted_file.write(decrypted_text)
 
         return render(request, 'encryption/decryption_success.html', {
